12e545b4254b3fa63068e8f9abaac0155281a921
David Hovemeyer <david.hovemeyer@gmail.com>
2017-02-23 13:21:20 -0500

    fixed hardest2.py to ignore subs from instructors
    modified demo.zip so that the one student is *not*
    an instructor

--- hardest2.py
+++ hardest2.py
@@ -1,5 +1,5 @@
 # Find the activity with the highest average number of
-# submissions per student.
+# submissions per student (omitting instructors)
 
 import sys, progsnap
 
@@ -13,17 +13,19 @@ for a in dataset.activities():
   total = 0
   n = 0
   for wh in dataset.work_histories_for_activity(a):
-    subs = [e for e in wh.events()
-            if type(e) is progsnap.Submission]
-    total += len(subs)
-    n += 1
-  avg = total / n
-  if avg > highest_avg:
-    activity = a
-    highest_avg = avg
+    student = dataset.student_for_id(wh.student_id())
+    if not student.instructor():
+      subs = [e for e in wh.events()
+              if type(e) is progsnap.Submission]
+      total += len(subs)
+      n += 1
+    avg = total / n
+    if avg > highest_avg:
+      activity = a
+      highest_avg = avg
 
 print("Activity {}, {} submissions/student"
-      .format(a.number(), highest_avg))
+      .format(activity.number(), highest_avg))
 
 # Note that the formatting of this file is somewhat
 # awkward because it was used in a poster where space
